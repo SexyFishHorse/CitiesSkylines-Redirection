dd20b2276b0faeb7ddd6f3d45fc32420107656ff
Asbjørn Hansen <ah@fenris.io>
2017-01-02 23:52:21 +0100

    added missing assembly redirector also by yenyang

---
+++ Redirection/AssemblyRedirector.cs
@@ -0,0 +1,41 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Redirection
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Attributes;
+    using Extensions;
+
+    public class AssemblyRedirector
+    {
+        private static Type[] types;
+
+        public static void Deploy()
+        {
+            types =
+                Assembly.GetExecutingAssembly()
+                        .GetTypes()
+                        .Where(t => t.GetCustomAttributes(typeof(TargetTypeAttribute), false).Length > 0)
+                        .ToArray();
+            foreach (var type in types)
+            {
+                type.Redirect();
+            }
+        }
+
+        public static void Revert()
+        {
+            if (types == null)
+            {
+                return;
+            }
+
+            foreach (var type in types)
+            {
+                type.Revert();
+            }
+
+            types = null;
+        }
+    }
+}
